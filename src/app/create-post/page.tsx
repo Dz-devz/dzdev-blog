965d90bc765156ef0a0e33ad6b93248be3d288c9
Dz.dev <darwinbjordan@gmail.com>
2024-06-11 12:03:42 +0800

    progress: added Authentication using kinde auth and added text area in create post

--- src/app/create-post/page.tsx
+++ src/app/create-post/page.tsx
@@ -1,21 +1,33 @@
 import { createPost } from "@/actions/actions";
+import Logout from "@/components/logout";
 
-export default function Page() {
+export default async function Page() {
   return (
     <main className="text-center pt-16">
       <h1 className="text-4xl md:text-5xl font-bold mb-5">Create post</h1>
-      <form action={createPost} className="h-10 space-x-2 mt-10">
+      <form
+        action={createPost}
+        className="gap-2 my-10 flex flex-col mx-auto max-w-[600px]"
+      >
         <input
-          className="border rounded px-3 h-full"
+          className="border rounded px-3 h-10"
           type="text"
           name="title"
           placeholder="Title for new post"
           required
         />
-        <button className="h-full px-5 rounded text-white bg-gray-500">
+        <textarea
+          name="body"
+          placeholder="New Post"
+          className="border rounded px-3 py-2"
+          rows={6}
+          required
+        />
+        <button className="h-10 px-5 rounded text-white bg-gray-500">
           Submit
         </button>
       </form>
+      <Logout />
     </main>
   );
 }
---
+++ src/components/logout.tsx
@@ -0,0 +1,6 @@
+"use client";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs";
+
+export default function Logout() {
+  return <LogoutLink>Log out</LogoutLink>;
+}
