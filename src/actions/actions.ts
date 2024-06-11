f53fde4b22593a825843fff8b395ade5c7479b3c
Dz.dev <darwinbjordan@gmail.com>
2024-06-11 12:42:50 +0800

    progress: added prisma for create && git push

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,6 +1,16 @@
 "use server";
 
+import prisma from "@/lib/db";
+
 export async function createPost(formData: FormData) {
-  const title = formData.get("title") as String;
-  console.log(title);
+  const title = formData.get("title") as string;
+  const body = formData.get("body") as string;
+  console.log(title, body);
+
+  await prisma.post.create({
+    data: {
+      title,
+      body,
+    },
+  });
 }
