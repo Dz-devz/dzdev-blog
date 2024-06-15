95cc8d4775cdea57680f060d7fac67474485885f
Dz.dev <darwinbjordan@gmail.com>
2024-06-15 21:37:07 +0800

    progress: added shadcn modal for edit to have an edit button

---
+++ src/components/modal-edit.tsx
@@ -0,0 +1,61 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import prisma from "@/lib/db";
+import { notFound } from "next/navigation";
+
+export async function ModalEdit({ params }: { params: { id: string } }) {
+  const post = await prisma.post.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
+  if (!post) {
+    notFound();
+  }
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Edit</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit</DialogTitle>
+          <DialogDescription>
+            Make changes to your Thoughts here. Click save when you are done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Title
+            </Label>
+            <Input
+              id="title"
+              defaultValue={post.title}
+              className="col-span-3"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
+              Thoughts
+            </Label>
+            <Input id="body" defaultValue={post.body} className="col-span-3" />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/components/single-post.tsx
+++ src/components/single-post.tsx
@@ -1,5 +1,6 @@
 import prisma from "@/lib/db";
 import { notFound } from "next/navigation";
+import { ModalEdit } from "./modal-edit";
 
 export default async function SinglePost({
   params,
@@ -18,6 +19,13 @@ export default async function SinglePost({
     <>
       <h1 className="font-semibold text-5xl mb-7">{post.title}</h1>
       <p className="max-w-[700px] mx-auto">{post.body}</p>
+      <div className="mt-20">
+        <ModalEdit
+          params={{
+            id: params.id,
+          }}
+        />
+      </div>
     </>
   );
 }
