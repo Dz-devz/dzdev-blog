890799fbe7ad9f090e2cf115185c866b3a70b726
Dz.dev <darwinbjordan@gmail.com>
2024-07-06 13:00:33 +0800

    chore: update about message

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,10 +3,10 @@ export default function Page() {
     <div className="flex items-center justify-center flex-col max-w-[700px] mx-auto">
       <h1 className="text-5xl mt-20  ">About DevThoughts</h1>
       <p className="leading-9 mt-10 text-2xl text-center">
-        Our goal with DevThoughts is to create a robust web application where
-        you can securely store your profound thoughts without fear of losing
-        them. We aim for DevThoughts to be flexible, catering to your personal
-        preference of storing Thoughts, and accessible from anywhere.{" "}
+        My goal with DevThoughts is to create a robust web application where you
+        can securely store your profound thoughts without fear of losing them.
+        We aim for DevThoughts to be flexible, catering to your personal
+        preference of storing idea, and accessible from anywhere.{" "}
       </p>
     </div>
   );
