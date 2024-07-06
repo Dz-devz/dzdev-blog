8479986a6fc69d800ad7e20fae2aba457724401f
Dz.dev <darwinbjordan@gmail.com>
2024-07-06 11:38:42 +0800

    progress: when creating post with same category it will not duplicate created link in category

--- src/components/category-link.tsx
+++ src/components/category-link.tsx
@@ -3,15 +3,27 @@ import Link from "next/link";
 
 export default async function CategoryLinks() {
   const allThoughts = await prisma.post.findMany();
+
+  // Use a Set to track unique categories
+  const categorySet: Set<string> = new Set();
+
+  // Populate the Set with unique category names
+  allThoughts.forEach((thought) => {
+    categorySet.add(thought.category);
+  });
+
+  // Convert Set to Array using Array.from
+  const uniqueCategories: string[] = Array.from(categorySet);
+
   return (
     <>
       <nav className="flex justify-center items-center py-4 px-4 text-zinc-400 gap-2 text-xl">
-        {allThoughts.map((categoryN) => (
-          <ul key={categoryN.id}>
-            <Link href={`/category-post/${categoryN.category}`}>
+        {/* Iterate over the Array of unique category names */}
+        {uniqueCategories.map((categoryName) => (
+          <ul key={categoryName}>
+            <Link href={`/category-post/${categoryName}`}>
               <li>
-                {categoryN.category.charAt(0).toUpperCase() +
-                  categoryN.category.slice(1)}
+                {categoryName.charAt(0).toUpperCase() + categoryName.slice(1)}
               </li>
             </Link>
           </ul>
