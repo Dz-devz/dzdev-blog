cc737e7977554b6183ceaaa46d6b4aedbdb3770e
Dz.dev <darwinbjordan@gmail.com>
2024-07-13 20:17:18 +0800

    fix: revert

--- src/actions/client-action.ts
+++ src/actions/client-action.ts
@@ -1,34 +1,34 @@
-// import toast from "react-hot-toast";
-// import { schema } from "../utils/schema";
-// import { createThoughts } from "./create-actions";
+import toast from "react-hot-toast";
+import { schema } from "../utils/schema";
+import { createThoughts } from "./create-actions";
 
-// export const clientAction = async (formData: FormData) => {
-//   const newThoughts = {
-//     category: formData.get("category"),
-//     title: formData.get("title"),
-//     body: formData.get("body"),
-//   };
+export const clientAction = async (formData: FormData) => {
+  const newThoughts = {
+    category: formData.get("category"),
+    title: formData.get("title"),
+    body: formData.get("body"),
+  };
 
-//   const result = schema.safeParse(newThoughts);
-//   if (!result.success) {
-//     let errorMessage = "";
+  const result = schema.safeParse(newThoughts);
+  if (!result.success) {
+    let errorMessage = "";
 
-//     result.error.issues.forEach((issue) => {
-//       errorMessage = errorMessage + issue.path[0] + ": " + issue.message + ". ";
-//     });
-//     toast.error(errorMessage);
-//     return;
-//   }
-//   console.log(result.data);
+    result.error.issues.forEach((issue) => {
+      errorMessage = errorMessage + issue.path[0] + ": " + issue.message + ". ";
+    });
+    toast.error(errorMessage);
+    return;
+  }
+  console.log(result.data);
 
-//   try {
-//     const response = await createThoughts(result.data);
-//     if (response?.error) {
-//       toast.error(response.error);
-//     } else {
-//       toast.success("Thoughts created successfully!");
-//     }
-//   } catch (error) {
-//     toast.error("Failed to create thoughts. Please try again later.");
-//   }
-// };
+  try {
+    const response = await createThoughts(result.data);
+    if (response?.error) {
+      toast.error(response.error);
+    } else {
+      toast.success("Thoughts created successfully!");
+    }
+  } catch (error) {
+    toast.error("Failed to create thoughts. Please try again later.");
+  }
+};
