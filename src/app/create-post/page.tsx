4e24bcc21b917dba2c1743167cc1a95f3617f23b
Dz.dev <darwinbjordan@gmail.com>
2024-07-19 18:24:54 +0800

    progress: added auth in create post

--- src/app/create-post/page.tsx
+++ src/app/create-post/page.tsx
@@ -1,7 +1,13 @@
 import Form from "@/components/form";
 import Logout from "@/components/logout";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 
 export default async function Page() {
+  const { isAuthenticated } = getKindeServerSession();
+  if (!(await isAuthenticated())) {
+    redirect("/api/auth/login");
+  }
   return (
     <main className="text-center pt-16">
       <h1 className="text-4xl md:text-5xl font-bold mb-5">
